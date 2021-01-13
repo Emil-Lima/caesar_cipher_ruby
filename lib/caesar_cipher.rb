9dae0defc112e546f51a8988f99bcbd7a0c8305f
J-Rozas <jrozaslima@gmail.com>
2021-01-13 22:23:52 +0000

    Added a folder to contain the main code.

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+def caesar_cipher(string, number)
+  string.split('')
+        .map { |letter| letter_or_not(letter, number) }
+        .join
+end
+
+def letter_or_not(character, number)
+  element = character.ord
+  base = element < 97 ? 65 : 97
+  if (element >= 65 && element <= 90) || (element >= 97 && element <= 122)
+    return (((element + number - base) % 26) + base).chr
+  end
+
+  character
+end
